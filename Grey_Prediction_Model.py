24815c511b8a2f1bd2011e18c0227305c9eb9443
FU-Long12138 <15045435783@163.com>
2022-12-07 21:55:05 +0800

    GM模型群与事前检验与事后检验已完成

--- Grey_Prediction_Model.py
+++ Grey_Prediction_Model.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class GM11:
+class GM11(object):
     def __init__(self, data):
         self.data = data
         self.AGO = None
@@ -23,6 +23,7 @@ class GM11:
         self.a, self.b = float(a_hat[0]), float(a_hat[1])
 
     def predict(self, pre_times):
+        self.model_fit()
         Times = len(self.data) + pre_times
 
         def pref(t, a=self.a, b=self.b, x0=self.data[0]):
@@ -35,52 +36,64 @@ class GM11:
         self.ori_hat = ori_hat
         return x0_pre
 
-    def evaluation(self, evaluation_index="Relative Error"):
-        e = self.data - self.ori_hat
-        re = np.abs(e / self.data)
-        re_mean = np.mean(re)
-        S1 = np.std(self.data, ddof=1)
-        S2 = np.std(e, ddof=1)
-        C0 = S2 / S1
-        Pe = np.mean(e)
-        p_error = sum(np.abs(self.data - self.ori_hat - Pe) < (0.6745 * S1)) / len(self.data)
-        if evaluation_index == "Relative Error":
-            if re_mean <= 0.01:
-                print("由相对误差判断模型预测精度为一级，相对误差为", re_mean)
-            elif 0.01 < re_mean <= 0.05:
-                print("由相对误差判断模型预测精度为二级，相对误差为", re_mean)
-            elif 0.05 < re_mean <= 0.10:
-                print("由相对误差判断模型预测精度为三级，相对误差为", re_mean)
-            elif 0.10 < re_mean <= 0.20:
-                print("由相对误差判断模型预测精度为四级，相对误差为", re_mean)
-            else:
-                print("不建议使用灰色预测模型，相对误差极大")
-            return re_mean
-        elif evaluation_index == "Mean Variance Ratio":
-            if C0 <= 0.35:
-                print("由均方差比值判断模型预测精度为一级，均方差比值为", C0)
-            elif 0.35 < C0 <= 0.5:
-                print("由均方差比值判断模型预测精度为二级，均方差比值为", C0)
-            elif 0.5 < C0 <= 0.65:
-                print("由均方差比值判断模型预测精度为三级，均方差比值为", C0)
-            elif 0.65 < C0 <= 0.8:
-                print("由均方差比值判断模型预测精度为四级，均方差比值为", C0)
-            else:
-                print("不建议使用灰色预测模型，均方差比值极大，均方差比值为", C0)
-            return C0
-        elif evaluation_index == "Probability of Small Error":
-            if p_error >= 0.95:
-                print("由小误差概率判断模型预测精度为一级，小误差概率为", p_error)
-            elif 0.8 < p_error <= 0.95:
-                print("由小误差概率判断模型预测精度为二级，小误差概率为", p_error)
-            elif 0.70 < p_error <= 0.8:
-                print("由小误差概率判断模型预测精度为三级，小误差概率为", p_error)
-            elif 0.6 < p_error <= 0.7:
-                print("由小误差概率判断模型预测精度为四级，小误差概率为", p_error)
-            else:
-                print("不建议使用灰色预测模型，小误差概率极小，小误差概率为", p_error)
-            return p_error
+
+class NewInformationGM11(GM11):
+    def __init__(self, data):
+        super().__init__(data)
+
+    def predict(self, pre_num):
+        pre_data = np.ones(pre_num)
+        Times = len(self.data)
+        for i in range(pre_num):
+            self.model_fit()
+
+            def pref(t, a=self.a, b=self.b, x0=self.data[0]):
+                return (x0 - b / a) * np.exp(-a * (t - 1)) + b / a
+
+            Times += 1
+            x1_hat = pref(np.arange(1, Times + 1))
+            x0_hat = np.hstack((x1_hat[0], np.diff(x1_hat)))
+            x0_pre = x0_hat[-1]
+            self.data = np.append(self.data, x0_pre)
+            pre_data[i] = x0_pre
+        return pre_data
 
 
-class Verhulst:
-    pass
+class MetabolismGM11(GM11):
+    def __init__(self, data):
+        super().__init__(data)
+
+    def predict(self, pre_num):
+        pre_data = np.ones(pre_num)
+        Times = len(self.data)
+        for i in range(pre_num):
+            self.model_fit()
+
+            def pref(t, a=self.a, b=self.b, x0=self.data[0]):
+                return (x0 - b / a) * np.exp(-a * (t - 1)) + b / a
+
+            Times += 1
+            x1_hat = pref(np.arange(1, Times + 1))
+            x0_hat = np.hstack((x1_hat[0], np.diff(x1_hat)))
+            x0_pre = x0_hat[-1]
+            self.data = np.delete(self.data, 0)
+            self.data = np.append(self.data, x0_pre)
+            pre_data[i] = x0_pre
+        return pre_data
+
+
+class Verhulst(GM11):
+    def __init__(self, data):
+        super().__init__(data)
+
+    def model_fit(self):
+        self.AGO = self.data.cumsum()
+        Z = []
+        for i in range(1, len(self.AGO)):
+            Z.append(0.5 * (self.AGO[i - 1] + self.AGO[i]))
+        self.Z = np.array(Z)
+        B = np.mat(np.vstack((-self.Z, self.Z**2))).T
+        Y = np.mat(np.delete(self.data, 0)).T
+        a_hat = np.linalg.inv(B.T * B) * B.T * Y
+        a_hat = np.array(a_hat)
+        self.a, self.b = float(a_hat[0]), float(a_hat[1])
--- Sequence_Check.py
+++ Sequence_Check.py
@@ -1,58 +1,117 @@
+# 对进行灰色预测的序列数据进行事前与事后检验
 import numpy as np
 
 
-def smooth_check(data):
-    """准光滑检验"""
-    AGO = data.cumsum()
-    if sum(data < 0) != 0:
-        print("准光滑检验序列不可有负数，请对序列做平移变换")
-    else:
-        rho = [data[i] / AGO[i - 1] for i in range(1, len(data))]
-        rhos = [rho[i - 1] / rho[i] for i in range(1, len(rho))]
-        per = []  # 符合准光滑检验数据的比例
-        pers = []
-        for i in rho:
-            if 0 < i < 0.5:
-                per.append(1)
-            else:
-                per.append(0)
-        for i in rhos:
-            if i < 1:
-                pers.append(1)
-            else:
-                pers.append(0)
-        if sum(per) == len(per) and sum(pers) == len(pers):
-            print('通过准光滑检验')
-            return True
+# 事前检验{准光滑检验、级比检验}
+def Before_Evaluation(data, evaluation_index="level_check", remove_set=False):
+    if evaluation_index == "level_check":
+        if sum(data < 0) != 0:
+            print('级比检验序列不可有负数，请对序列数据做平移变换')
         else:
-            print('未通过准光滑检验')
-            print('通过准光滑检验的数据占比为：', round(sum(per) / len(per), 2))
-            return False
-
-
-def level_check(data):
-    """级比检验"""
-    if sum(data < 0) != 0:
-        print('级比检验序列不可有负数，请对序列数据做平移变换')
-    else:
-        n = len(data)
-        lambdas = []
-        trues = []
-        for i in range(1, n):
-            lambdai = data[i - 1] / data[i]
-            if np.exp(-2 / (n + 1)) < lambdai < np.exp(2 / (n + 1)):
-                trues.append(1)
+            n = len(data)
+            lambdas = []
+            trues = []
+            for i in range(1, n):
+                lambdai = data[i - 1] / data[i]
+                if np.exp(-2 / (n + 1)) < lambdai < np.exp(2 / (n + 1)):
+                    trues.append(1)
+                else:
+                    trues.append(0)
+                lambdas.append(lambdai)
+            if 0 in trues:
+                print('级比检验不通过，请对序列数据做平移变换')
+                return False
             else:
-                trues.append(0)
-            lambdas.append(lambdai)
-        if 0 in trues:
-            print('级比检验不通过，请对序列数据做平移变换')
-            return False
+                print('级比检验通过')
+                return True
+    elif evaluation_index == "smooth_check":
+        AGO = data.cumsum()
+        if sum(data < 0) != 0:
+            print("准指数检验序列不可有负数，请对序列做平移变换")
         else:
-            print('级比检验通过')
-            return True
+            rho = [data[i] / AGO[i - 1] for i in range(1, len(data))]
+            rhos = [rho[i - 1] / rho[i] for i in range(1, len(rho))]
+            per = []  # 符合准指数检验数据的比例
+            pers = []
+            for i in rho:
+                if 0 < i < 0.5:
+                    per.append(1)
+                else:
+                    per.append(0)
+            for i in rhos:
+                if i < 1:
+                    pers.append(1)
+                else:
+                    pers.append(0)
+            if remove_set:
+                if (sum(per[2:]) / (len(per) - 2)) > 0.6:
+                    print("通过准指数检验")
+                    print('去掉前两个数据后通过准光滑检验的数据占比为：', round(sum(per[2:]) / (len(per) - 2), 2))
+                    return True
+                else:
+                    print("未通过准指数检验")
+                    print('去掉前两个数据后通过准光滑检验的数据占比为：', round(sum(per[2:]) / (len(per) - 2), 2))
+                    return False
+            else:
+                if (sum(per) / len(per)) > 0.9:
+                    print("通过准指数检验")
+                    print('通过准指数检验的数据占比为：', round(sum(per) / len(per), 2))
+                    return True
+                else:
+                    print("未通过准指数检验")
+                    print('通过准指数检验的数据占比为：', round(sum(per) / len(per), 2))
+                    return False
 
 
-def translation(data, n=5):
-    """对不满足级比检验的序列数据做平移变换"""
+def translation(data, fuc, n=5, max_iter=500):
+    """对不满足事前检验的序列数据做平移变换"""
     pass
+
+
+# 事后检验, {相对误差、均方差比值、小误差概率、灰色关联度}
+def After_Evaluation(true_data, hat_data, evaluation_index="Relative Error"):
+    """对灰色预测进行事后检验, {相对误差检验、均方差比检验、小误差概率检验}"""
+    e = true_data - hat_data
+    re = np.abs(e / true_data)
+    re_mean = np.mean(re)
+    S1 = np.std(true_data, ddof=1)
+    S2 = np.std(e, ddof=1)
+    C0 = S2 / S1
+    Pe = np.mean(e)
+    p_error = np.sum(np.abs(e - Pe) < (0.6745 * S1)) / len(e)
+    if evaluation_index == "Relative Error":
+        if re_mean <= 0.01:
+            print("由相对误差判断模型预测精度为一级，相对误差为", re_mean)
+        elif 0.01 < re_mean <= 0.05:
+            print("由相对误差判断模型预测精度为二级，相对误差为", re_mean)
+        elif 0.05 < re_mean <= 0.10:
+            print("由相对误差判断模型预测精度为三级，相对误差为", re_mean)
+        elif 0.10 < re_mean <= 0.20:
+            print("由相对误差判断模型预测精度为四级，相对误差为", re_mean)
+        else:
+            print("不建议使用灰色预测模型，相对误差极大")
+        return re_mean
+    elif evaluation_index == "Mean Variance Ratio":
+        if 0 <= C0 < 0.35:
+            print("由均方差比值判断模型预测精度为一级，均方差比值为", C0)
+        elif 0.35 <= C0 < 0.5:
+            print("由均方差比值判断模型预测精度为二级，均方差比值为", C0)
+        elif 0.5 <= C0 < 0.65:
+            print("由均方差比值判断模型预测精度为三级，均方差比值为", C0)
+        elif 0.65 <= C0 <= 0.8:
+            print("由均方差比值判断模型预测精度为四级，均方差比值为", C0)
+        else:
+            print("不建议使用灰色预测模型，均方差比值极大，均方差比值为", C0)
+        return C0
+    elif evaluation_index == "Probability of Small Error":
+        if 0.95 < p_error <= 1:
+            print("由小误差概率判断模型预测精度为一级，小误差概率为", p_error)
+        elif 0.8 < p_error <= 0.95:
+            print("由小误差概率判断模型预测精度为二级，小误差概率为", p_error)
+        elif 0.70 < p_error <= 0.8:
+            print("由小误差概率判断模型预测精度为三级，小误差概率为", p_error)
+        elif 0.6 < p_error <= 0.7:
+            print("由小误差概率判断模型预测精度为四级，小误差概率为", p_error)
+        else:
+            print("不建议使用灰色预测模型，小误差概率极小，小误差概率为", p_error)
+        return p_error
